25f1692ed3602792ffd2f251b36d6a05c07608dd
Mohamed Egal <v-moegal@microsoft.com>
2019-07-11 10:18:22 -0700

    Initial Commit

---
+++ TwoButtons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoButtons
+//
+//  Created by Mohamed Egal on 7/11/19.
+//  Copyright © 2019 Mohamed Egal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
